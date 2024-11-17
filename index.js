d79e65ad8e473b2e9a6f8ff4446025c4de869d4b
danoy <danoyalpha@gmail.com>
2024-11-17 13:37:31 +0000

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path')
 
 const server = express()
-server.post('/service',(req,res)=>res.send('<h2>Welcome to the service page</h2>'))
-server.put('/about',(req,res)=>res.send('<h2>Welcome to the about page</h2>'))
-server.get('/',(req,res)=>res.send('<h2>Welcome to the home page</h2>'))
-server.delete('/',(req,res)=>res.send('<h2>Welcome to the home page</h2>'))
+
+server.use(express.static(path.join(__dirname,'public')))
+
+// server.use('/')
 server.listen(3000,'127.0.0.1',()=>console.log('Server is ready to receive request'))
